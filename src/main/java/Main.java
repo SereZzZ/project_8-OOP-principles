fc405fcabd2caa258c60af2766d0863a1fee75a9
Neto sergey test <serez.gavriliv@mail.ru>
2024-02-24 12:35:02 +0300

    Initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Сергей";
+        post.passport = "4444 № 333333";
+        post.patronymic = "Гаврилив";
+        post.phone = "+7 (904)-026-14-49";
+        post.surname = "Гаврилив";
+        post.subscription = true;
+        post.birthday.day = 13;
+        post.birthday.month = 10;
+        post.birthday.year = 1999;
+
+    }
+}
